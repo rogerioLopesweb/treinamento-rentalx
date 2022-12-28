f2cc0979d6765ca33a32b2fe4bd8f00590f0bed8
Rogerio Lopes <rogerio@one.com>
2022-12-28 20:20:02 -0300

    upload treinamento ignite rocktsead

---
+++ readme.md
@@ -0,0 +1,20 @@
+## Comandos
+
+* yarn init -y
+* yarn add express
+* yarn add @types/typescript -D
+* yarn add typescript -D
+* yarn tsc --init
+* yarn add ts-node-dev --dev
+* yarn add eslint@^7.21.0 -D
+* yarn lint
+* yarn add -D @typescript-eslint/eslint-plugin@latest eslint-config-airbnb-base@latest eslint-plugin-import@^2.22.1 @typescript-eslint/parser@latest
+* yarn add -D eslint-plugin-import-helpers eslint-import-resolver-typescript
+* yarn add prettier eslint-config-prettier eslint-plugin-prettier -D
+* yarn add uuid
+* yarn add @types/uuid -D
+
+
+## Instalar VS Code ESLint extension
+* https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+
---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {SpecificationsRepository } from '../modules/cars/repositories/SpecificationsRepository';
+import {CreateSpecificationsService } from '../modules/cars/services/CreateSpecificationsService';
+const specificationsRoutes =  Router();
+const specificationsRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+    const { name,  description } = request.body;
+    const createSpecificationsService = new CreateSpecificationsService(specificationsRepository);
+    createSpecificationsService.execute({name,  description});
+    return response.status(201).send();
+});
+specificationsRoutes.get("/", (request, response) => {
+    const { name,  description } = request.body;
+    const all = specificationsRepository.list();
+    return response.status(201).json(all);
+});
+
+export { specificationsRoutes };
+
