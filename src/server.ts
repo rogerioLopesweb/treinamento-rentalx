3b5c28ec4bf248ee5b75c951d011fc4af8ff6e9f
Rogerio Lopes <rogerio@one.com>
2023-01-18 10:10:33 -0300

    organizaçõa de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
+
+
 const app = express();
 app.use(express.json()); // ativar o recebimento de json na api
-app.use("/categories",categoriesRoutes);
-app.use("/specifications",specificationsRoutes);
-
+app.use(router); // add as rotas
 
 
 app.listen(3333, () => console.log("Server is running!"));
