8428dd7b60f9ee57909d5675f6fca831b4568426
Rogerio Lopes <113315029+rogerioLopesweb@users.noreply.github.com>
2023-01-31 16:49:23 -0300

    Correções de atualização

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,17 +1,22 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import 'dotenv/config'
+import "reflect-metadata";//TypeORM dependencia 
+import { DataSource } from "typeorm";
+
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: ["./src/modules/**/entities/*.ts"],
+    migrations: ["./src/database/migrations/*.ts"],
+});
+
+export function createConnection(
+    host = process.env.NODE_ENV === "test" ? "localhost" : "database"
+  ): Promise<DataSource> {
+    return AppDataSource.setOptions({ host }).initialize()
+  }
+  
