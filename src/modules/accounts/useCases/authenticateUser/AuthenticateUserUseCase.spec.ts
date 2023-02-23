5507f2f3bf82bbf1900dcd6fe226dec2427d6541
Rogerio Lopes <113315029+rogerioLopesweb@users.noreply.github.com>
2023-02-23 11:30:40 -0300

    create test Authenticate user

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,70 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryinMemory } from "../../repositories/in-memory/UsersRepositoryinMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryinMemory: UsersRepositoryinMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe("Authenticate User", () => {
+    beforeAll(() =>{
+        usersRepositoryinMemory = new UsersRepositoryinMemory();
+        authenticateUserUseCase =  new AuthenticateUserUseCase(usersRepositoryinMemory);
+        createUserUseCase =  new CreateUserUseCase(usersRepositoryinMemory);
+    });
+    
+    it("shoold be able to authenticate an user", async () => {
+        const user: ICreateUserDTO = {
+            name: "User Test", 
+            email: "user@test.com", 
+            password: "123456", 
+            driver_license: "001231"
+        };
+
+        await createUserUseCase.execute(user);
+
+        const result = await authenticateUserUseCase.execute({
+            email: user.email,
+            password: user.password
+        });
+
+        expect(result).toHaveProperty("token");
+
+    });
+
+    it("shoold not be able to authenticate an nonexistent user", async () => {
+        
+        expect( async () => {
+            const result = await authenticateUserUseCase.execute({
+                email: "false@email.com",
+                password: "1254"
+            });
+        }).rejects.toBeInstanceOf(AppError);
+
+    });
+
+    it("shoold not be able to authenticate with incorrect password", async () => {
+        
+        expect( async () => {
+            const user: ICreateUserDTO = {
+                name: "User Test Error", 
+                email: "user@test.com", 
+                password: "1234576", 
+                driver_license: "009999"
+            };
+            await createUserUseCase.execute(user);
+
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: "incorrectPassord"
+            });
+
+        }).rejects.toBeInstanceOf(AppError);
+
+    });
+
+
+
+})
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/AppError";
 
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
@@ -29,12 +30,12 @@ class AuthenticateUserUseCase {
     async execute({email, password} : IRequest): Promise<IResponse> {
        const user = await this.userRepository.findByEmail(email);
        if(!user){
-         throw new Error("Email or password incorrect!");
+         throw new AppError("Email or password incorrect!");
        }
 
        const passwordMatch = await compare(password, user.password);
        if(!passwordMatch){
-        throw new Error("Email or password incorrect!");
+        throw new AppError("Email or password incorrect!");
        }
        const token = sign({}, "ef0fd34af5eecb4319d40158db5b92b8", {
         subject: user.id,
